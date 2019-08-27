766a06f0d51129063eee6d8c5f0a63799ef36b6e
Eric Diaz <ericdiaz@pursuit.org>
2019-08-26 22:55:21 -0400

    Added RecyclerView implementation

---
+++ app/src/main/java/ericdiaz/program/geniusplazacodingchallenge/view/recyclerview/UsersAdapter.java
@@ -0,0 +1,37 @@
+package ericdiaz.program.geniusplazacodingchallenge.view.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import ericdiaz.program.geniusplazacodingchallenge.R;
+import ericdiaz.program.geniusplazacodingchallenge.model.User;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersViewHolder> {
+
+    private User[] users;
+
+    public UsersAdapter(User[] users) {
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_item_view, parent, false);
+        return new UsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UsersViewHolder holder, int position) {
+        holder.onBind(users[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.length;
+    }
+}
