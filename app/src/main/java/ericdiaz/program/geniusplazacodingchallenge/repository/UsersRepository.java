955c229e3d9bb79e8aec5d4f81591390a2c1a5b1
Eric Diaz <ericdiaz@pursuit.org>
2019-08-26 22:04:24 -0400

    Created UserViewModel with network  call method

--- app/src/main/java/ericdiaz/program/geniusplazacodingchallenge/repository/UsersRepository.java
+++ app/src/main/java/ericdiaz/program/geniusplazacodingchallenge/repository/UsersRepository.java
@@ -4,9 +4,17 @@ import ericdiaz.program.geniusplazacodingchallenge.model.UsersResponse;
 import ericdiaz.program.geniusplazacodingchallenge.network.RetrofitServiceGenerator;
 import io.reactivex.Flowable;
 
+/**
+ * A repository for conducting network call to User API
+ * <p>
+ * Created 8/26/19
+ *
+ * @author Eric Diaz
+ */
+
 public final class UsersRepository {
 
-    public Flowable<UsersResponse> getUsers(final int page) {
-        return RetrofitServiceGenerator.getUserService().getUsers(page);
+    public Flowable<UsersResponse> getUsers(final int pageNumber) {
+        return RetrofitServiceGenerator.getUserService().getUsers(pageNumber);
     }
 }
